f4a5c1596851aeaeef5c8c2e10a534b3edf395ab
mingchaotang <mingchao.tang@gmail.com>
2020-04-07 14:35:48 -0400

    wrap up projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import CardList from "./components/card-list/card-list.component";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component"
 import "./App.css";
 
 class App extends Component {
@@ -7,8 +8,13 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
 
   componentDidMount() {
@@ -18,9 +24,16 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()
+      ));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './card.styles.css'
 
-const Card = (props) => (
+export const Card = (props) => (
     <div className="card-container">
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
         <h2 key={props.monster.id}>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
 )
-
-export default Card;
